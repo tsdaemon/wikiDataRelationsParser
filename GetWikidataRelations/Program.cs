7c8a0a99aa04a3ece8ddced481067fa5b5088219
Anatoliy Stegniy <tsdaemon@gmail.com>
2016-06-15 16:47:34 +0300

    properties list

---
+++ GetWikidataRelations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetWikidataRelations
+{
+    class Program
+    {
+        const string GET_RELATIONS_QUERY = 
+        static void Main(string[] args)
+        {
+        }
+
+        // person       Q4047087
+        // organization Q43229
+        // location     Q17334923
+        // instance of  P31
+        static void GetAllRelationTypesForTypes(string[] types, string outFile)
+        {
+            var pairs = new List<string[]>();
+            for (var i = 0; i < types.Length; i++)
+            {
+                for (var j = 0; j < types.Length; j++)
+                {
+                    var pair = new[] {types[i], types[j]};
+                    pairs.Add(pair);
+                }
+            }
+
+
+        }
+    }
+}
