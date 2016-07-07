23834983182f644394392347d9249e448a4ab246
Anatoliy Stegniy <tsdaemon@gmail.com>
2016-07-07 18:14:06 +0300

    test article read

---
+++ GetWikiArticle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Core.Wikifile;
+
+namespace GetWikiArticle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new WikidumpReader("D:\\DB\\ukwiki\\ukwiki-20160601-pages-articles.xml"))
+            {
+                var article = reader.ExtractArticleText("Менделєєв Дмитро Іванович");
+                using (var streamWriter = new StreamWriter(File.OpenWrite("../../715_orig_sharp.txt")))
+                {
+                    streamWriter.Write(article);
+                }
+            }
+        }
+    }
+}
